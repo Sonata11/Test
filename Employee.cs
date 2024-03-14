96b3360651be1be3c415e8669dbec52e6af67ff6
Sonata11 <162658099+Sonata11@users.noreply.github.com>
2024-03-14 16:13:08 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreCrudDemo.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string Name { get; set; }      
+        public string Email { get; set; }
+        public string Address { get; set; }      
+        public string Phone { get; set; }
+    }
+}
