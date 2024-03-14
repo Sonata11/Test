b4fabc05b95c3e712a6c7ee6a2e782dfcad3bcc9
Sonata11 <162658099+Sonata11@users.noreply.github.com>
2024-03-14 10:59:47 +0530

    Add files via upload

---
+++ 20240116012810_cheque.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VendorAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class cheque : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CancelChequeBlobName",
+                table: "VendorBlob",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CancelChequeBusinessName",
+                table: "VendorBlob",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CancelChequeNumber",
+                table: "VendorBlob",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CancelChequeURL",
+                table: "VendorBlob",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelChequeBlobName",
+                table: "VendorBlob");
+
+            migrationBuilder.DropColumn(
+                name: "CancelChequeBusinessName",
+                table: "VendorBlob");
+
+            migrationBuilder.DropColumn(
+                name: "CancelChequeNumber",
+                table: "VendorBlob");
+
+            migrationBuilder.DropColumn(
+                name: "CancelChequeURL",
+                table: "VendorBlob");
+        }
+    }
+}
